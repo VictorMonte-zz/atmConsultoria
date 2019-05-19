d107bd96973d64226af4f676eee76fe2b4563a82
Victor Monte <victorbcm@hotmail.com>
2019-05-19 16:27:55 -0300

    Add images to cena principal

--- src/components/CenaPrincipal.js
+++ src/components/CenaPrincipal.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
-import { View, StatusBar } from 'react-native';
+import { 
+    View, 
+    StatusBar,
+    Image
+} from 'react-native';
 
 import BarraNavegacao from './BarraNavegacao';
+
+const logo = require('./../../imgs/logo.png');
+const menuCliente = require('./../../imgs/menu_cliente.png');
+const menuContato = require('./../../imgs/menu_contato.png');
+const menuEmpresa = require('./../../imgs/menu_empresa.png');
+const menuServico = require('./../../imgs/menu_servico.png');
 
 export default class CenaPrincipal extends Component {
     render() {
@@ -13,6 +23,13 @@ export default class CenaPrincipal extends Component {
                 />
 
                 <BarraNavegacao />  
+
+                <Image source={logo} />
+                <Image source={menuCliente} />
+                <Image source={menuContato} />
+                <Image source={menuEmpresa} />
+                <Image source={menuServico} />
+
             </View>
         );
     }
